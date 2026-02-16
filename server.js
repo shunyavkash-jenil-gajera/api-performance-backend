c6de5d8aa5b51216742aa95751791a25f51ef1f9
jenilGajera <jenil.gajera@shunyavkash.com>
2026-02-16 17:18:44 +0530

    add some. credentials

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const SUPPORTED_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://api-performance-frontend.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
